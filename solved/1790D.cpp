7319cc8de67bcac0b44942dd2c34f5d0c31f6c00
bh1rg1v <manukondabhargavram@gmail.com>
2025-10-01 01:58:39 +0530

    Updated start.cpp - default template :)

--- solved/1790D.cpp
+++ solved/1790D.cpp
@@ -1,33 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-
-const int INF = 1e9;
-const ll LINF = 1e18;
-const int MOD = 1e9+7;
-
-#define pb push_back
-#define mp make_pair
-#define all(v) (v).begin(), (v).end()
-#define f first
-#define s second
-#define sz(x) (int)(x).size()
-
-#define MAX(v) (*max_element((v).begin(), (v).end()))
-#define MIN(v) (*min_element((v).begin(), (v).end()))
-
-#define OFREQ(v) ([](auto &vec){ map<decltype(vec[0]), int> m; for (auto &x : vec) m[x]++; return m; }(v))
-#define FREQ(v) ([](auto &vec){ unordered_map<decltype(vec[0]), int> m; for (auto &x : vec) m[x]++; return m; }(v))
-
-#define OSET(v) (set<decltype(v[0])>(v.begin(), v.end()))
-#define SET(v) (unordered_set<decltype(v[0])>(v.begin(), v.end()))
-
 #define MAX_HEAP(T) priority_queue<T>
 #define MIN_HEAP(T) priority_queue<T, vector<T>, greater<T>>
 
@@ -96,9 +69,24 @@ int main() {
 void solve() {
 
     int n; cin >> n; vector<int> nums(n); for (int i = 0; i < n; i++) cin >> nums[i];
-
     // int n, k; cin >> n >> k; vector<int> nums(n, 0); for (int i = 0; i < n; i++) cin >> nums[i];
-    // int n, m; cin >> n >> m; vector<vector<int>> mat(n, vector<int>(m)); for(int i=0;i<n;i++) for(int j=0;j<m;j++) cin >> mat[i][j];
 
+    map<int, int> freq;
+    set<int> s;
+
+    for (int x : nums) {
+        freq[x]++;
+        s.insert(x);
+    }
+
+    int last = 0;
+    int ans = 0;
+
+    for (int num : s) {
+        ans += max(0, freq[num] - freq[num - 1]);
+        last = num;
+    }
+
+    cout << ans << endl;
 
 }
