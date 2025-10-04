b3ade551fb4da16e294d7f4f8075bb327a9f46e5
bh1rg1v <manukondabhargavram@gmail.com>
2025-10-05 02:25:19 +0530

    Solved 4 new problems

---
+++ 1582C.cpp
@@ -0,0 +1,151 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9+7;
+
+#define pb push_back
+#define mp make_pair
+#define all(v) (v).begin(), (v).end()
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+
+#define pans cout << ans << endl
+#define pyes cout << "YES" << endl
+#define pno cout << "NO" << endl
+#define pm1 cout << -1 << endl
+#define pvec(v) for (auto x : v) cout << x << " "; cout << "\n";
+#define int long long
+
+#define MAX(v) (*max_element((v).begin(), (v).end()))
+#define MIN(v) (*min_element((v).begin(), (v).end()))
+#define SUM(v) accumulate((v).begin(), (v).end(), 0LL)
+
+#define OFREQ(v) ([](auto &vec){ map<decltype(vec[0]), int> m; for (auto &x : vec) m[x]++; return m; }(v))
+#define FREQ(v) ([](auto &vec){ unordered_map<decltype(vec[0]), int> m; for (auto &x : vec) m[x]++; return m; }(v))
+
+#define OSET(v) (set<decltype(v[0])>(v.begin(), v.end()))
+#define SET(v) (unordered_set<decltype(v[0])>(v.begin(), v.end()))
+
+#define MAX_HEAP(T) priority_queue<T>
+#define MIN_HEAP(T) priority_queue<T, vector<T>, greater<T>>
+
+bool isMidValid(vector<int>& nums, int mid) {
+
+        return true;
+
+    }
+
+int maxiMin(vector<int>& nums, int k) {
+
+    int n = nums.size();
+
+    int low = 0;
+    int high = (int)1e9;
+
+    while (low <= high) {
+
+        int mid = low + (high - low) / 2;
+
+        if (isMidValid(nums, mid)) low = mid;
+        else high = mid - 1;
+    }
+
+    return low;
+}
+
+int miniMax(vector<int>& nums, int k) {
+
+    int n = nums.size();
+    int ans = -1;
+
+    int low = 0;
+    int high = (int)1e9;
+
+    while (low <= high) {
+
+        int mid = low + (high - low) / 2;
+
+        if (isMidValid(nums, mid)) {
+            ans = mid;
+            high = mid + 1;
+        } else {
+            low = mid + 1;
+        }
+
+    }
+
+    return high;
+}
+
+void solve();
+
+int32_t main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+void solve() {
+
+    // int n; cin >> n; vector<int> a(n); for (int i = 0; i < n; i++) cin >> a[i];
+    int n; cin >> n; string s; cin >> s;
+
+    // int n, k; cin >> n >> k; vector<int> nums(n, 0); for (int i = 0; i < n; i++) cin >> nums[i];
+    // int n, m; cin >> n >> m; vector<vector<int>> mat(n, vector<int>(m)); for(int i=0;i<n;i++) for(int j=0;j<m;j++) cin >> mat[i][j];
+
+    set<char> chars;
+    for (char c : s) chars.insert(c);
+
+    int ans = INT_MAX;
+
+    for (char c : chars){
+        int l = 0;
+        int r = n - 1;
+
+        int ops = 0;
+        bool flag = true;
+
+        while (l <= r){
+            if (s[l] == s[r]){
+                l++;
+                r--;
+            } else if (s[l] != c and s[r] != c) {
+                flag = false;
+                break;
+            } else if (s[l] == c) {
+                ops += 1;
+                l++;
+            } else if (s[r] == c) {
+                ops+= 1;
+                r--;
+            }
+        }
+
+        if (flag) {
+            ans = min(ans, ops);
+        }
+    }
+
+    if (ans == INT_MAX){
+        pm1;
+    } else {
+        pans;
+    }
+
+}
---
+++ 1635C.cpp
@@ -0,0 +1,137 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9+7;
+
+#define pb push_back
+#define mp make_pair
+#define all(v) (v).begin(), (v).end()
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+
+#define pans cout << ans << endl
+#define pyes cout << "YES" << endl
+#define pno cout << "NO" << endl
+#define pm1 cout << -1 << endl
+#define pvec(v) for (auto x : v) cout << x << " "; cout << "\n";
+#define int long long
+
+#define MAX(v) (*max_element((v).begin(), (v).end()))
+#define MIN(v) (*min_element((v).begin(), (v).end()))
+#define SUM(v) accumulate((v).begin(), (v).end(), 0LL)
+
+#define OFREQ(v) ([](auto &vec){ map<decltype(vec[0]), int> m; for (auto &x : vec) m[x]++; return m; }(v))
+#define FREQ(v) ([](auto &vec){ unordered_map<decltype(vec[0]), int> m; for (auto &x : vec) m[x]++; return m; }(v))
+
+#define OSET(v) (set<decltype(v[0])>(v.begin(), v.end()))
+#define SET(v) (unordered_set<decltype(v[0])>(v.begin(), v.end()))
+
+#define MAX_HEAP(T) priority_queue<T>
+#define MIN_HEAP(T) priority_queue<T, vector<T>, greater<T>>
+
+bool isMidValid(vector<int>& nums, int mid) {
+
+        return true;
+
+    }
+
+int maxiMin(vector<int>& nums, int k) {
+
+    int n = nums.size();
+
+    int low = 0;
+    int high = (int)1e9;
+
+    while (low <= high) {
+
+        int mid = low + (high - low) / 2;
+
+        if (isMidValid(nums, mid)) low = mid;
+        else high = mid - 1;
+    }
+
+    return low;
+}
+
+int miniMax(vector<int>& nums, int k) {
+
+    int n = nums.size();
+    int ans = -1;
+
+    int low = 0;
+    int high = (int)1e9;
+
+    while (low <= high) {
+
+        int mid = low + (high - low) / 2;
+
+        if (isMidValid(nums, mid)) {
+            ans = mid;
+            high = mid + 1;
+        } else {
+            low = mid + 1;
+        }
+
+    }
+
+    return high;
+}
+
+void solve();
+
+int32_t main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+void solve() {
+
+    int n; cin >> n; vector<int> a(n); for (int i = 0; i < n; i++) cin >> a[i];
+
+    // int n, k; cin >> n >> k; vector<int> nums(n, 0); for (int i = 0; i < n; i++) cin >> nums[i];
+    // int n, m; cin >> n >> m; vector<vector<int>> mat(n, vector<int>(m)); for(int i=0;i<n;i++) for(int j=0;j<m;j++) cin >> mat[i][j];
+
+    if (a[n - 2] > a[n - 1]) {
+        pm1;
+        return;
+    }
+
+    vector<int> nums = a;
+    sort(all(nums));
+
+    if (equal(a.begin(), a.end(), nums.begin())){
+        cout << 0 << endl;
+        return;
+    }
+
+    if (a[n - 1] >= 0) {
+
+        cout << n - 2 << endl;
+
+        for (int i = 1; i <= n - 2; i++){
+            cout << i << " " << n - 1 << " " << n << endl;
+        }
+
+    } else {
+        pm1;
+        return;
+    }
+
+}
